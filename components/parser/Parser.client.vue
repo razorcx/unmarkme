5aa69587745cb2980476435130905238a21c198e
Cory LePage <cory@razorcx.com>
2023-09-29 15:04:00 -0700

    fix padding on convert columns

--- components/parser/Parser.client.vue
+++ components/parser/Parser.client.vue
@@ -4,14 +4,21 @@
       <UnmarkmeNav class="z-[999] bg-white" />
       <main class="flex flex-wrap md:flex-nowrap space-y-24 md:space-y-0 mt-10 ">
         <div
-          class="flex flex-col space-y-5 w-full items-center mx-auto md:border-r-2"
+          class="flex flex-col space-y-5 w-full items-center mx-auto md:border-r-2 md:pr-6"
         >
           <div class="prose w-full px-4 md:px-6 lg:px-0">
             <h2>HTML to Markdown</h2>
             <h3>HTML</h3>
-            <p>Type or Copy/Clip your HTML here 👇</p>
+            <div class="flex justify-between items-center">
+              <p>Type or Copy/Clip your HTML here 👇</p>
+              <NuxtLink
+                to="https://developer.mozilla.org/en-US/docs/Learn/HTML/Introduction_to_HTML/Getting_started" class="hover:text-slate-600"
+                >HTML Guide</NuxtLink
+              >
+            </div>
             <textarea
               v-model="html"
+              placeholder="Type HTML here"
               class="w-full h-64 p-6 border overflow-auto shadow-md"
             >
             </textarea>
@@ -23,14 +30,23 @@
             <div v-html="backToHtml" class="border px-6 py-4 shadow-md"></div>
           </div>
         </div>
-        <div class="flex flex-col space-y-5 w-full items-center mx-auto">
+        <div class="flex flex-col space-y-5 w-full items-center mx-auto md:pl-6">
           <div class="prose w-full px-4 md:px-6 lg:px-0">
             <h2>Markdown to HTML</h2>
             <h3>Markdown</h3>
-            <p>Type or Copy/Clip your Markdown here 👇</p>
+            <div class="flex justify-between items-center">
+              <p>Type or Copy/Clip your Markdown here 👇</p>
+              <NuxtLink
+                to="https://www.markdownguide.org/basic-syntax"
+                class="hover:text-slate-600"
+                >Markdown Guide</NuxtLink
+              >
+            </div>
+
             <textarea
               v-model="markdown"
-              class="w-full h-64 p-6 border overflow-auto shadow-md" 
+              placeholder="Type Markdown here"
+              class="w-full h-64 p-6 border overflow-auto shadow-md"
             >
             </textarea>
             <h3>HTML</h3>
@@ -39,9 +55,10 @@
             </div>
             <h3>Back to Markdown</h3>
             <div>
-              <pre class="prose border p-6 bg-white text-black rounded-none shadow-md">{{
-                backToMarkdown
-              }}</pre>
+              <pre
+                class="prose border p-6 bg-white text-black rounded-none shadow-md"
+                >{{ backToMarkdown }}</pre
+              >
             </div>
           </div>
         </div>
@@ -52,14 +69,16 @@
 </template>
 
 <script setup>
-const sampleHtml = `<h1>This is H1 Header</h1>
+const sampleHtml = `***Sample HTML***
+<h1>This is H1 Header</h1>
 <h2>This is H2 Header</h2>
 <h3>This is H3 Header</h3>
 <h4>This is H4 Header</h4>
 <p>Hello World!</p>
 `;
 
-const sampleMarkdown = `This is unMarkme
+const sampleMarkdown = `***Sample Markdown***
+This is unMarkme
 =========
 unMarkme is awesome`;
 
